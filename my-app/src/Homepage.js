0d0d01e8680d29eebefe937b6cdd4a8035c68e64
mia24sims <167013397+mia24sims@users.noreply.github.com>
2025-11-06 22:30:13 -0600

    Added home and about section
    integrated bootstrap

---
+++ my-app/src/Homepage.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Homepage.css";
+
+
+export default function Homepage() {
+  return (
+    <div className="Homepage">
+      <h1>Mia Sims</h1>
+      <h3>Frontend Developer </h3>
+      <p className="bio">
+        I like to create front-end products with seamless designs to give
+        users a cultivating experience.
+      </p>
+    <hr />
+    </div>
+  );
+}
