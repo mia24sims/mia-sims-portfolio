0bfa1fbd534804498ceda7388b3f3e22d189a58e
mia24sims <167013397+mia24sims@users.noreply.github.com>
2025-11-08 00:26:21 -0600

    update parent foler

---
+++ src/About.js
@@ -0,0 +1,43 @@
+import React from "react";
+import "./About.css";
+
+export default function About() {
+  return (
+    <div className="About">
+      <h2>A little about me..</h2>
+
+      <div class="container text-center">
+        <div class="row">
+          <div class="col-md-2 dev">
+            <h4> Frontend Developer</h4>
+            <p>I enjoy building and designing from a blank canvas</p>
+            <h6>My languages / Tools</h6>
+            <ul>
+              <li> React </li>
+              <li>HTML CSS Javascript</li>
+              <li> VS Code</li>
+              <li>Netlify</li>
+              <li>Bootstrap</li>
+              <li>Github</li>
+            </ul>
+          </div>
+
+          <div class="col-md-2 edu">
+            <h4> Experience </h4>
+            <p>
+              I am breaking into tech after taking phenomenal online courses
+            </p>
+            <h6>Education</h6>
+
+            <ul>
+              <li>shecodes workshop </li>
+              <li>code-cademy</li>
+              <li>degree*</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <hr/>
+    </div>
+  );
+}
---
+++ src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+import Homepage from "./Homepage";
+import About from "./About";
+import Contact from "./Contact";
+import Work from "./Work";
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+    < Homepage />
+<About />
+<Contact />
+<Work />
+      </header>
+    </div>
+  );
+}
+
+export default App;
